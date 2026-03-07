7a00fbb18ce93b6b918349b16903b55e3d84987b
Alexander Schischkin <schischkin2@yandex.com>
2026-03-07 18:04:03 +0100

    Исправил импорты в utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 import json
-from typing import List, Dict, Union
+from typing import Dict, List
+
 
 def load_operations(file_path: str) -> List[Dict]:
     """
     Загружает операции из JSON-файла
     """
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if isinstance(data, list):
                 return data
