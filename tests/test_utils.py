f3408c61fdcd0a38a9b5a46d08e58dd3db24d36e
Alexander Schischkin <schischkin2@yandex.com>
2026-03-07 19:08:33 +0100

    обработка тестов

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,16 @@
 import pytest
-from src.utils import (
-    # перечислите здесь все функции из utils.py
+
+from src.utils import (  # перечислите здесь все функции из utils.py
+    another_function,
     example_function,
-    another_function
 )
+
 
 def test_example_function():
     # Пример теста для первой функции
     result = example_function(input_data)
     assert result == expected_output
+
 
 def test_another_function():
     # Пример теста для второй функции
