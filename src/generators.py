ffcc2c572b53f6d9c1ba7d3f7937d5830b3beee4
Alexander Schischkin <schischkin2@yandex.com>
2026-03-10 10:47:55 +0100

    добавил Аннотации типов для параметров

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,8 @@
-def filter_by_currency(transactions, currency):
+from typing import Generator, Dict, List, Any
+
+def filter_by_currency(
+    transactions: List[Dict[str, Any]], currency: str
+) -> Generator[Dict[str, Any], None, None]:
     """
     Фильтрует транзакции по заданной валюте
 
@@ -12,7 +16,9 @@ def filter_by_currency(transactions, currency):
             print(f"Found transaction with currency {currency}")  # Для отладки
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(
+    transactions: List[Dict[str, Any]]
+) -> Generator[str, None, None]:
     """
     Возвращает описания транзакций
 
@@ -23,7 +29,9 @@ def transaction_descriptions(transactions):
         yield transaction.get("description", "Нет описания")
 
 
-def card_number_generator(start, stop):
+def card_number_generator(
+    start: int, stop: int
+) -> Generator[str, None, None]:
     """
     Генерирует номера карт в заданном диапазоне
 
